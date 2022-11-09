8dca57790bc14cf02fcdf9b43ebae341918dd8d2
Lulu <hoste324@gmail.com>
2022-11-09 17:11:39 +0100

    added comments and cleaned up code

--- mqtt.py
+++ mqtt.py
@@ -1,82 +1,70 @@
 import random
-import paho.mqtt.client as mqtt
-from serial_communication import powerSupply
-from threading import Thread
+import paho.mqtt.client as mqtt # paho mqtt om te publishen en te subscriben naar/op de mqtt broker
+from serial_communication import powerSupply # om scpi commands naar de voeding te kunnen sturen
+from threading import Thread # om threads te kunnen starten
 import time
-import math
-import matplotlib.pyplot as plt
-
-power = powerSupply('COM5')
-
-randomCurrent = False
-randomVoltage = False
+import math # voor de sinus te kunnen berekenen
+import matplotlib.pyplot as plt # om de sinus te plotten
 
 
+
+power = powerSupply('COM5') # object van de klasse powerSupply, met als poort COM5 (windows), op linux is dit iets zoals /dev/ttyUSB0
+
+randomCurrent = False # om te controleren of het sinus effect mag blijven loopen
+
+# wat te doen bij een connectie met een broker
 def on_connect(client, userdata, flags, rc):
     # This will be called once the client connects
     print(f"Connected with result code {rc}")
-    # Subscribe here!
+    # Subscribe op de verschillende broker topics
     client.subscribe("voltage")
     client.subscribe("current")
     client.subscribe("state")
     client.subscribe("randomVoltage")
     client.subscribe("currentEffect")
-    client.subscribe("voltageEffect")
 
-
+# wat te doen wanneer een message binnenkomt
 def on_message(client, userdata, msg):
-    global randomCurrent
-    global randomVoltage
-    print(f"Message received [{msg.topic}]: {msg.payload}")
-    if(msg.topic == 'state'):
+    global randomCurrent # zorgt ervoor dat we aan de globale variabele kunnen binnen deze functie
+    print(f"Message received [{msg.topic}]: {msg.payload}") # print van de message die binnengekomen is en de topic van deze message
+    if(msg.topic == 'state'): # wat te doen als de topic state is
       if(msg.payload.decode().strip() == "off"):
-        power.turnOff()
+        power.turnOff() # zet de voeding uit
       elif(msg.payload.decode().strip() == "on"):
-        power.turnOn()
+        power.turnOn() # zet de voeding aan
     elif(msg.topic == 'voltage'):
-      # voltage = msg.payload.decode().strip()
-      # print("detected voltage " +voltage)
-      # print(voltage.isnumeric())
       voltage = msg.payload.decode().strip()
-      if(voltage.isnumeric() and (voltage <= 45 or voltage >= 22)):
-        print('voltage message received')
-        power.setVoltage(voltage)
+      if(voltage.isnumeric() and (voltage <= 45 or voltage >= 22)): # houd de voltage waarde tussen 22 en 45 anders gebeurt er niets, ook controle of wel een getal ontvangen werd
+        print('voltage message received') 
+        power.setVoltage(voltage) # zet de voltage van de voeding op de ontvangen waarde
     elif(msg.topic == 'current'):
       current = msg.payload.decode().strip()
-      # print("received current after decode:" + current)
-      if(float(current) and (current <= 10 or current >= 1)):
+      if(float(current) and (current <= 10 or current >= 1)): # houd de voltage waarde tussen 1 en 10 anders gebeurt er niets, ook controle of wel een getal ontvangen werd
         print("current message received after float check")
-        power.setCurrent(current)
+        power.setCurrent(current) # zet de current van de voeding op de ontvangen waarde
     elif(msg.topic == 'currentEffect'):
       if(msg.payload.decode().strip() == "off"):
         print("turn off effect received")
-        randomCurrent = False
+        randomCurrent = False # stopt de loop van het sinus effect
       elif(msg.payload.decode().strip() == "on"):
-        randomCurrent = True
-        print("turn on effect received")
-    elif(msg.topic == 'voltageEffect'):
-      if(msg.payload.decode().strip() == "off"):
-        print("turn off effect received")
-        randomVoltage = False
-      elif(msg.payload.decode().strip() == "on"):
-        randomVoltage = True
+        randomCurrent = True # start de loop van het sinus effect
         print("turn on effect received")
     
 def calculateSine(input):
-  return round((2.5*math.sin(input-7.9)) + 3.5,1)
+  return round((2.5*math.sin(input-7.9)) + 3.5,1) # berekent de sinus aan de hand van de gegeven input
 
-def publishCurrentWave(client, currentWave):
+def publishCurrentWave(client, currentWave): # publisht de huidige waarde van de sinus naar de broker en zet ook de current van de voeding naar deze waarde
   while(True):
     while(randomCurrent):
         for i in range (currentWave.length):
-          time.sleep(5)
+          time.sleep(5) # update elke 5 seconden
           number = currentWave[i]
           power.setCurrent(number)
           client.publish("current",number)
-          if(not randomCurrent):
+          if(not randomCurrent): # stopt deze loop wannneer randomCurrent false wordt
             break
 
-def startMqtt(client):
+def startMqtt(client): # functie om de mqtt loop te starten
   client.loop_forever()
 
 # Start by calculating the sine wave we want the current to follow
@@ -86,13 +74,13 @@ nightLength = (1*60)//5
 # print(length)
 
 output = []
-for i in range(dayLength):
+for i in range(dayLength): # output gedurende de "dag"
   output.append(calculateSine(i/7.5))
 
-for i in range(nightLength):
+for i in range(nightLength): # output gedurende de nacht = 1
   output.append(1)
 
-plt.plot(output, color="red")
+plt.plot(output, color="red") # plot van de sinus (inclusief nacht)
 plt.show()
 
 client = mqtt.Client("python_app") # client ID "mqtt-test"
@@ -101,13 +89,14 @@ client.on_message = on_message
 client.username_pw_set("mqttuser", "lab1234")
 client.connect('172.16.101.121', 1883)
 
+# alle settings voor de connectie met de mqtt broker
+
 # Start networking daemon
-mqttThread = Thread(target=startMqtt,args=[client])
-effectsThread = Thread(target=publishCurrentWave, args=[client])
-# client.loop_forever() # loops in this thread and blocks everything else ?
-print("after starting mqtt")
-mqttThread.start()
-effectsThread.start()
+mqttThread = Thread(target=startMqtt,args=[client]) # de mqtt thread
+effectsThread = Thread(target=publishCurrentWave, args=[client]) # de sinus thread
+
+mqttThread.start() # start de mqtt thread
+effectsThread.start() # start de sinus thread
 
 
 
--- serial_communication.py
+++ serial_communication.py
@@ -1,49 +1,43 @@
 
-import time
-import serial
+import time # time voor sleeps
+import serial # serial zorgt voor communicatie aan de hand van scpi commands
 import random
 
 
 class powerSupply:
 
-  def __init__(self, serialPort):
+  def __init__(self, serialPort): # maakt instantie van klasse aan met port gelijk aan de input
     self.conn = serial.Serial(port= serialPort, baudrate=115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, 
     stopbits=serial.STOPBITS_ONE, timeout=1)
 
-  def setVoltage(self, voltage):
+  def setVoltage(self, voltage): # set de voltage
     voltageMessage = 'VOLTage {}<LF>\n'.format(voltage)
-    self.conn.write(voltageMessage.encode())
+    self.conn.write(voltageMessage.encode()) # write de message naar de voeding
 
   def setCurrent(self, current):
     voltageMessage = 'CURRent {}<LF>\n'.format(current)
-    self.conn.write(voltageMessage.encode())
+    self.conn.write(voltageMessage.encode()) # write de message naar de voeding
 
   def getVoltage(self):
     message = 'MEASure:VOLTage?\n'
-    self.conn.write(message.encode())
+    self.conn.write(message.encode()) # write de message naar de voeding
 
   def getCurrent(self):
     message = 'MEASure:CURRent?\n'
-    self.conn.write(message.encode())
+    self.conn.write(message.encode()) # write de message naar de voeding
 
   def turnOff(self):
-    message = b'OUTput OFF\r\n'
-    self.conn.write(message)
+    message = b'OUTput OFF\r\n' # werkt niet met .encode, maar wel als binary string
+    self.conn.write(message) # write de message naar de voeding
 
   def turnOn(self):
-    message = b'OUTput ON\r\n'
-    self.conn.write(message)
+    message = b'OUTput ON\r\n' # werkt niet met .encode, maar wel als binary string
+    self.conn.write(message) # write de message naar de voeding
 
-  def setRandomCurrent(self):
+  def setRandomCurrent(self): # zet de stroom van de voeding iedere 5 seconden op een random waarde
     self.setCurrent(round(random.uniform(1,10),1))
     time.sleep(5)
 
-  def setRandomVoltage(self):
+  def setRandomVoltage(self): # zet de spanning van de voeding iedere 5 seconden op een random waarde
     self.setVoltage(random.randint(22,45))
     time.sleep(5)
-
-# power = powerSupply('COM5')
-# power.randomVoltage()
-# # time.sleep(20)
-# # power.stopRandomVoltage()
-# print("random voltage done")
